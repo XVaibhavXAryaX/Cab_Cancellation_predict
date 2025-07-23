2d5dcc1272c357fe5c5f502a8da59c98413ec4ef
XVaibhavXAryaX <vkarya711@gmail.com>
2025-07-23 19:11:31 +0530

    commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+
